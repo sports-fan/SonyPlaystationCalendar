8d18144cf422ae9f5d9e72a30df162be8c18ac16
sports-fan <lovesports801@gmail.com>
2024-02-01 05:48:07 -0600

    Fix some styling issues and refactor

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { startOfMonth, endOfMonth, eachDayOfInterval, format, getMonth, getYear, getDate } from 'date-fns'
+import { startOfMonth, endOfMonth, eachDayOfInterval, format, getDate, getMonth, getYear } from 'date-fns';
 
 import eventsData from './events.json';
 import './styles.css';
@@ -10,13 +10,17 @@ const Calendar = () => {
   const { year, month } = useParams();
 
   const [isValidDate, setIsValidDate] = useState(true);
-  const [events, setEvents] = useState([])
+  const [events, setEvents] = useState([]);
+  const [showEvent, setShowEvent] = useState(false);
+  const [currentEvent, setCurrentEvent] = useState({});
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (month > 12) setIsValidDate(false)
+  const WEEK_NAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+  useEffect(() => {
+    if (month > 12 || month < 1) setIsValidDate(false);
+    // fetch event from the http://amock.io
     const fetchEvents = async () => {
       try {
         const response = await axios.get('http://amock.io/');
@@ -28,77 +32,117 @@ const Calendar = () => {
 
     fetchEvents();
     const validEvents = eventsData.reduce((acc, data) => {
-      const isValid = getMonth(new Date(data.launchDate)) + 1 === parseInt(month) && getYear(new Date(data.launchDate)) === parseInt(year)
+      const isValid = 
+        getMonth(new Date(data.launchDate)) + 1 === parseInt(month) && getYear(new Date(data.launchDate)) === parseInt(year);
 
-      if(isValid) {
+      if (isValid) {
         acc.push({
           date: getDate(data.launchDate),
           detail: data
-        })
+        });
       }
 
-      return acc
-    }, [])
+      return acc;
+    }, []);
 
-    setEvents(validEvents)
+    setEvents(validEvents);
+    setCurrentEvent({});
+    setShowEvent(false);
   }, [month, year]);
 
-  if (!isValidDate) {
-    setIsValidDate(true)
-    const date = new Date()
-    navigate(`/${year}/${date.getMonth() + 1}`);
-  }
+  useEffect(() => {
+    if (!isValidDate) {
+      setIsValidDate(true);
+      const date = new Date();
+      navigate(`/${getYear(date)}/${getMonth(date) + 1}`);
+    }
+  }, [isValidDate, navigate]);
+
 
   const daysInMonth = useMemo(() => eachDayOfInterval({
     start: startOfMonth(new Date(year, month - 1)),
     end: endOfMonth(new Date(year, month - 1))
   }), [year, month]);
 
-  const getMonthName = (monthNumber) => {
+  const daysInMonthWithEvent = useMemo(() => {
+    const ary = [];
+    for (let i = 0; i < daysInMonth.length; i++) {
+      if (i === 14) ary.push('event');
+      ary.push(daysInMonth[i]);
+    }
+    return ary;
+  }, [daysInMonth]);
+
+  const getMonthName = useCallback((monthNumber) => {
     const date = new Date(year, monthNumber - 1);
     return format(date, 'MMMM');
-  };
+  }, [year]);
 
   const handleNextMonth = useCallback(() => {
     const newYear = month === '12' ? parseInt(year) + 1 : year;
     const newMonth = month === '12' ? 1 : parseInt(month) + 1;
 
     navigate(`/${newYear}/${newMonth}`);
-  }, [year, month, navigate])
+  }, [year, month, navigate]);
 
   const handlePrevMonth = useCallback(() => {
     const newYear = month === '1' ? parseInt(year) - 1 : year;
     const newMonth = month === '1' ? 12 : parseInt(month) - 1;
 
     navigate(`/${newYear}/${newMonth}`);
-  }, [year, month, navigate])
-
-  const WEEK_NAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  }, [year, month, navigate]);
 
   const getEventDataOfDate = useCallback((date) =>
     events.filter((event) => event.date === getDate(date))[0]
-  , [events])
+  , [events]);
+
+  const handleEvent = useCallback((date) => {
+    const event = getEventDataOfDate(date);
+    if (!event) {
+      setCurrentEvent({});
+      setShowEvent(false);
+      return;
+    }
+
+    setCurrentEvent(event.detail);
+    setShowEvent(true);
+  }, [getEventDataOfDate]);
 
   return (
     <div className='container'>
-      <div className='currentMonth'>
-        <div className='prevMonth' onClick={handlePrevMonth}>❮</div>
-        <div className='currentMonthTitle'>{getMonthName(month)} {year}</div>
-        <div className='nextMonth' onClick={handleNextMonth}>❯</div>
+      <div className='current-month'>
+        <div className='prev-month' onClick={ handlePrevMonth }>❮</div>
+        <div className='current-month-title'>{ getMonthName(month) } {year}</div>
+        <div className='next-month' onClick={ handleNextMonth }>❯</div>
       </div>
-      <div className='calendarGrid'>
+      <div className='calendar-grid'>
         {WEEK_NAMES.map((name, idx) => (
-          <div className='weekNames' key={idx}>
-            {name}
+          <div className='week-names' key={idx}>
+            { name }
           </div>
         ))}
-        {daysInMonth.map((day, idx) =>
-          <div key={idx} className='day'>
-            {!!getEventDataOfDate(day) && <img src={`/images/${getEventDataOfDate(day).detail.imageFilenameThumb}.webp`} alt="thumb" className='game-thumb' />}
-            <div className='day-number'>
-              <div className={!!getEventDataOfDate(day) && 'active'}>{day.getDate()}</div>
+        {daysInMonthWithEvent.map((day, idx) => idx === 14 ?
+          showEvent ? (
+            <div className='event-full'>
+              <div className='event-content' style={{ backgroundImage: `url(/images/${currentEvent.imageFilenameFull}.webp)` }} >
+                <div className='event-main'>
+                  <div className='event-content-title'>{ currentEvent.title.toUpperCase() }</div>
+                  <div className='event-content-summary'>{ currentEvent.summary }</div>
+                  <div className='event-content-date'>Available { format(new Date(currentEvent.launchDate), 'MMMM dd yyyy') }</div>
+                </div>
+                <div className='event-action'>
+                  <a type='button' className='event-content-learn' href={currentEvent.learnMoreLink}>Learn More</a>
+                  <a type='button' className='event-content-order' href={currentEvent.purchaseLink}>Pre Order Now</a>
+                </div>
+              </div>
             </div>
-          </div>)}
+          ) : null :
+          (<div key={idx} className='day' onClick={() => handleEvent(day)}>
+            {!!getEventDataOfDate(day) && <img src={`/images/${getEventDataOfDate(day).detail.imageFilenameThumb}.webp`} alt="thumb" className='event-thumb' />}
+            <div className='day-number'>
+              <div className={!!getEventDataOfDate(day) && 'active'}>{ getDate(day) }</div>
+            </div>
+          </div>))}
       </div>
     </div>
   )
