267b6b75f2c44b46019fec043ee5d9e04bed5c92
sports-fan <lovesports801@gmail.com>
2024-01-31 18:25:20 -0600

    Fetch events and show then on calendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,17 +1,47 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { startOfMonth, endOfMonth, eachDayOfInterval, format } from 'date-fns'
+import axios from 'axios';
+import { startOfMonth, endOfMonth, eachDayOfInterval, format, getMonth, getYear, getDate } from 'date-fns'
 
+import eventsData from './events.json';
 import './styles.css';
 
 const Calendar = () => {
-  const [isValidDate, setIsValidDate] = useState(true);
   const { year, month } = useParams();
+
+  const [isValidDate, setIsValidDate] = useState(true);
+  const [events, setEvents] = useState([])
+
   const navigate = useNavigate();
 
   useEffect(() => {
     if (month > 12) setIsValidDate(false)
-  }, [year, month]);
+
+    const fetchEvents = async () => {
+      try {
+        const response = await axios.get('http://amock.io/');
+        return response.data;
+      } catch (error) {
+        console.error('Error fetching events', error);
+      }
+    };
+
+    fetchEvents();
+    const validEvents = eventsData.reduce((acc, data) => {
+      const isValid = getMonth(new Date(data.launchDate)) + 1 === parseInt(month) && getYear(new Date(data.launchDate)) === parseInt(year)
+
+      if(isValid) {
+        acc.push({
+          date: getDate(data.launchDate),
+          detail: data
+        })
+      }
+
+      return acc
+    }, [])
+
+    setEvents(validEvents)
+  }, [month, year]);
 
   if (!isValidDate) {
     setIsValidDate(true)
@@ -35,34 +65,40 @@ const Calendar = () => {
 
     navigate(`/${newYear}/${newMonth}`);
   }, [year, month, navigate])
-  
+
   const handlePrevMonth = useCallback(() => {
     const newYear = month === '1' ? parseInt(year) - 1 : year;
     const newMonth = month === '1' ? 12 : parseInt(month) - 1;
-  
+
     navigate(`/${newYear}/${newMonth}`);
   }, [year, month, navigate])
 
   const WEEK_NAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
+  const getEventDataOfDate = useCallback((date) =>
+    events.filter((event) => event.date === getDate(date))[0]
+  , [events])
+
   return (
     <div className='container'>
       <div className='currentMonth'>
-        <div className='prevMonth' onClick={ handlePrevMonth }>❮</div>
-        <div className='currentMonthTitle'>{ getMonthName(month) } { year }</div>
-        <div className='nextMonth' onClick={ handleNextMonth }>❯</div>
+        <div className='prevMonth' onClick={handlePrevMonth}>❮</div>
+        <div className='currentMonthTitle'>{getMonthName(month)} {year}</div>
+        <div className='nextMonth' onClick={handleNextMonth}>❯</div>
       </div>
       <div className='calendarGrid'>
         {WEEK_NAMES.map((name, idx) => (
           <div className='weekNames' key={idx}>
-            { name }
+            {name}
           </div>
         ))}
-        {daysInMonth.map((day, idx) => (
+        {daysInMonth.map((day, idx) =>
           <div key={idx} className='day'>
-            { day.getDate() }
-          </div>
-        ))}
+            {!!getEventDataOfDate(day) && <img src={`/images/${getEventDataOfDate(day).detail.imageFilenameThumb}.webp`} alt="thumb" className='game-thumb' />}
+            <div className='day-number'>
+              <div className={!!getEventDataOfDate(day) && 'active'}>{day.getDate()}</div>
+            </div>
+          </div>)}
       </div>
     </div>
   )
